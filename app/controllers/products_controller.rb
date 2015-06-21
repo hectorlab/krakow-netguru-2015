9ab6787796ed44900b5b39737777835c85e8e2f7
hectorlab <hectorlab@outlook.com>
2015-06-21 14:21:13 +0200

    Permissions products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :require_login, only: [:create, :update]
+
   expose(:category)
   expose(:products)
   expose(:product)
@@ -15,36 +17,55 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    return if current_user == product.user
+    redirect_to category_product_url(category, product)
+    flash[:error] = 'You are not allowed to edit this product.'
   end
 
   def create
     self.product = Product.new(product_params)
+    product.user_id = current_user.id
 
     if product.save
       category.products << product
-      redirect_to category_product_url(category, product), notice: 'Product was successfully created.'
+      redirect_to category_product_url(category, product),
+      notice: 'Product was successfully created.'
     else
       render action: 'new'
     end
   end
 
   def update
-    if self.product.update(product_params)
-      redirect_to category_product_url(category, product), notice: 'Product was successfully updated.'
+    if current_user == product.user
+      if product.update(product_params)
+        redirect_to category_product_url(category, product),
+        notice: 'Product was successfully updated.'
+      else
+        render action: 'edit'
+      end
     else
-      render action: 'edit'
+      redirect_to category_product_url(category, product)
+      flash[:error] = 'You are not allowed to edit this product.'
     end
   end
 
-  # DELETE /products/1
   def destroy
     product.destroy
-    redirect_to category_url(product.category), notice: 'Product was successfully destroyed.'
+    redirect_to category_url(product.category),
+    notice: 'Product was successfully destroyed.'
   end
 
   private
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :category_id)
+  end
+
+  def require_login
+    redirect_to new_user_session_path unless logged_in?
+  end
+
+  def logged_in?
+    !current_user.nil?
   end
 end
