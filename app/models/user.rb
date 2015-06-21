5247baab23f8c09811af2e2334c8961276bff09b
hectorlab <hectorlab@outlook.com>
2015-06-21 13:10:09 +0200

    Solved - first issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :firstname, presence: true
+  validates :lastname, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +8,14 @@ class User < ActiveRecord::Base
 
   has_many :reviews
   has_many :products
+
+  def user_params
+    params.require(:user)
+    .permit(:firstname, :lastname, :email, :password, :password_confirmation, :remember_me)
+  end 
+
+  def admin?
+    admin
+  end
+
 end
